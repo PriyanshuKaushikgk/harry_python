839760dfebec2e511163b421d34f4efe987dc730
priyanshu kaushik <priyanshukaushikdk@gmail.com>
2024-07-02 23:15:53 +0530

    fist upload

---
+++ String.py
@@ -0,0 +1,25 @@
+# string is a data type in python  and string is immutable so not to be changed .
+
+
+
+# string is sequence of characters enclosed in quotes.
+
+# a = 'harry'    # single quote string
+# b = "hello"    # double quote string
+# c = '''hey'''   # triple quote string
+
+
+#----------String Slicing--------------------
+
+# A string in python can be sliced for getting a part of the string.
+
+name  = "Priyanshu"
+
+print(name[-4:-1])
+print(name[1:4])
+print(name[:4])
+print(name[1:])
+print(name[1:5])
+
+
+
