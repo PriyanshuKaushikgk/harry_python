56e2c59251bb51e7ec71e3505ff59d7dcc8c56f0
priyanshu kaushik <priyanshukaushikdk@gmail.com>
2024-07-03 22:57:20 +0530

    update

---
+++ tuple.py
@@ -0,0 +1,18 @@
+# tuple can not be changed
+
+# a = (1,23,4,45,6,False,"Rohan")
+
+# print(a)
+
+
+# ----------Tuple Method-------------
+
+a = (1,2,3,3212,242,45,23454,"Rohan",False,45)
+
+print(a)
+no = a.count(45)
+print(no)
+
+
+i = a.index(45)
+print(i)
