ec2579b9c69ff10e7ae4bf8abea59cd83beb8c85
priyanshu kaushik <priyanshukaushikdk@gmail.com>
2024-07-03 23:00:33 +0530

    main

---
+++ list_tuple.py
@@ -0,0 +1,30 @@
+# list = Python list are container to store a set of any data type.
+
+# friends = ["Apple","Orange",5,30.25,"Akash","Radha"]
+
+# print(friends[0])
+# friends[0] = "Grapes" # unlike Strings lists are mutable
+
+
+# print(friends[0])
+
+# print(friends[1:4])
+
+
+#list method----------
+# friends = ["Apple","Orange",5,30.25,"Akash","Radha"]
+# print(friends)
+
+# friends.append("HArry")
+
+# print(friends)
+
+
+l1 = [1,34,5,355,254,698]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3,33333)    #   insert 33333 suchthat its index in the list is 3
+# print(l1.pop(3))
+print(l1.remove(5))
+print(l1)
+
