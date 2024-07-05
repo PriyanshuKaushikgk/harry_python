9468000795ed4db5b7687e0c8b08c0fa83d5b2a0
priyanshu kaushik <priyanshukaushikdk@gmail.com>
2024-07-05 12:48:13 +0530

    new

---
+++ ch1 practise set/prob5.py
@@ -0,0 +1,63 @@
+# Marks = []
+
+# f1 = int(input("Enter Marks name:"))
+# Marks.append(f1)
+# f2 = int(input("Enter the Marks nam)e:")
+# Marks.append(f2)
+# f3 = int(input("Enter Marks:")
+# Ma)rks.append(f3)
+# f4 = int(input("enter the marks her)e:")
+# Marks.append(f4)
+# f5= int(input("enter the marks her)e:")
+# Marks.append(f5)
+# f6 = int(input("enter the marks her)e:")
+# Marks.append(f6)
+# f7 = int(input("enter the marks her)e:")
+# Marks.append(f7)
+
+
+# print(Marks)
+
+# ------------display 6 students marks---------
+
+# Marks = []
+
+# f1 = int(input("Enter Marks here:"))
+# Marks.append(f1)
+# f2 = int(input("Enter the Marks here"))
+# Marks.append(f2)
+# f3 = int(input("Enter Marks:"))
+# Marks.append(f3)
+# f4 = int(input("enter the marks here:"))
+# Marks.append(f4)
+# f5= int(input("enter the marks here:"))
+# Marks.append(f5)
+# f6 = int(input("enter the marks here:"))
+# Marks.append(f6)
+
+
+
+# print(Marks)
+
+
+#--------check tuple type can not be changed--------------
+
+# a = (34,234,"harry")
+
+# a[2]  = "lary"     # tuple is immutable.
+
+
+# ---------
+
+
+# l = [34,54,63,671]
+
+# print(sum(l))
+
+
+#-----------------------------
+
+a = (7,0,5,0,6,0,8,0)
+
+n = a.count(0)
+print(n)
---
+++ sets&_dictionary.py
@@ -0,0 +1,16 @@
+#  -----------dictionary--------
+
+marks = {
+    "harry": 100,
+    "shubham":56,
+    "rohan":23
+}
+
+# print(marks,type(marks))
+# print(marks["harry"])
+# print(marks.values())
+# marks.update({"harry":89,"renuka":99})
+# print(marks)
+
+
+print(marks.get("harry"))
