a7e8fda96f60d9c800867f33eacf73e452427afb
priyanshu kaushik <priyanshukaushikdk@gmail.com>
2024-07-09 19:16:12 +0530

    update

---
+++ conditional_statements.py
@@ -0,0 +1,52 @@
+# a = int(input("enter your age:"))
+
+# if(a>=18):
+#     print("eligible for vote!")
+#     print("Good for you")
+
+# elif(a<=0):
+#     print("invalid negative age")
+
+# else:
+#     print("not eligible ")
+
+
+# print("end your program")
+
+
+#--------------------
+# a = int(input("Enter your Age:"))
+
+# if(a>=18):
+#     print("yes ")
+
+# else:
+#     print("not")
+
+# print("End program!")
+
+#--------------MULTIPLE PROGRAMS--------------
+a = int(input("Enter your age:"))
+
+# if statement no.1
+
+if(a%2==0):
+    print("a is even")
+
+#if statement no.2
+
+if(a%2==0):
+    print("you are the above the age of consent")
+    print("Good for you")
+
+elif(a<0):
+    print("You are entering an invalid negative age")
+
+else:
+    print("You are below the age of consent")
+
+#End of if statement no.2
+
+print("End of Program")
+
+
