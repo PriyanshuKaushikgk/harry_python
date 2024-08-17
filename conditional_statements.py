b762566b3523cf93bb36c5d60f84322252dc7136
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-08-17 11:16:59 +0530

    Add files via upload

--- conditional_statements.py
+++ conditional_statements.py
@@ -50,3 +50,4 @@ else:
 print("End of Program")
 
 
+
---
+++ functions.py
@@ -0,0 +1,23 @@
+#function definition
+# def avg():
+#     a = int(input("Enter your number:"))
+#     b = int(input("Enter your number:"))
+#     c = int(input("Enter your number:"))
+
+#     average = (a+b+c)/3
+#     print(average)
+# avg()    # Function call
+# avg()
+# avg()
+# print("Thank you!")
+# avg()
+# avg()
+
+# -----------------------------QUICK  QUIZ-------------------
+
+def goodbay(name, ending):
+    print("good Day," + name +" " + ending)
+    # print(ending)
+
+goodbay("harry" , "thank you")
+goodbay("Priyanshu", "thanks")
